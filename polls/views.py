9a14fe0610cc58bc550538355abeb709f426bc00
Leah <ldctaylor@gmail.com>
2023-07-23 21:50:49 +1000

    Switch to class based views

--- polls/views.py
+++ polls/views.py
@@ -1,40 +1,43 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
+#class based view
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+            return Question.objects.order_by("-pub_date")[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-         "latest_question_list":latest_question_list,
-         }
-    return render(request, "polls/index.html", context)
+# FUNCTION BASED VIEW
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {
+#          "latest_question_list":latest_question_list,
+#          }
+#     return render(request, "polls/index.html", context)
 
-# this was the original version before I rewrote it using render():
-    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # template = loader.get_template("polls/index.html")
-    # context = {
-    #     "latest_question_list": latest_question_list,
-    # }
-    # return HttpResponse(template.render(context,request))
+class DetailView(generic.DetailView):
+     model = Question
+     template_name = "polls/detail.html"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/detail.html", {"question": question}) 
-
-#detail view with long way of doing 404:
+# FUNCTION BASED DETAIL VIEW
 # def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question Does Not Exist")
-#     return render(request,"polls/detail.html", {"question": question})
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,"polls/detail.html", {"question": question}) 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,"polls/results.html",{"question":question})
+class ResultsView(generic.DetailView):
+     model = Question
+     template_name = "polls/results.html"
+
+#FUNCTION BASED RESULTS VIEW
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,"polls/results.html",{"question":question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
