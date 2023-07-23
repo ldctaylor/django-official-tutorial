fde62100f1988939d6e6e803a10d7c242e160f82
Leah <ldctaylor@gmail.com>
2023-07-23 20:46:04 +1000

    Refine views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404
-from django.template import loader
 
 
 from .models import Question
@@ -8,7 +7,9 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
+    context = {
+         "latest_question_list":latest_question_list,
+         }
     return render(request, "polls/index.html", context)
 
 # this was the original version before I rewrote it using render():
@@ -37,14 +38,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request,question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("you're voiting on question %s." % question_id)
